55a74236796462917dfd3a4c0ddb0258312767e2
yytdfc <fuchen@foxmail.com>
2018-03-15 10:41:24 +0800

    add some test

--- test/test-exd.cc
+++ test/test-exd.cc
@@ -2,7 +2,14 @@
 #include <iostream>
 
 int main(){
-  std::cout << "";
+  // exd::split
+  std::string s = ",,33,32s,";
+  std::cout << exd::split(s,",") << std::endl;
+  std::cout << exd::split("1,2,3",",") << std::endl;
+  std::cout << exd::strip(s,",") << std::endl;
+  std::cout << exd::strip("    ,") << std::endl;
+
+  // exd::join
   std::vector<int> aa {1, 3, 4};
   std::cout << aa << std::endl;
   std::vector<std::string> a {"af", "123"};
@@ -10,6 +17,8 @@ int main(){
   std::cout << exd::split(std::string("fad af af")) << std::endl;
   std::string str = "One Two Three ...";
   std::cout << exd::join(exd::split(str), "#")  << std::endl;
+
+  // oss map
   std::map<std::string, int> map1;
   map1["something"] = 69;
   map1["anything"] = 199;
